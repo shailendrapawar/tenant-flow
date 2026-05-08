07f07aa74d4f41314f4fe3331a04ab77bcafeab9
shailendrapawar07 <shailendrapawar.axe@gmail.com>
2026-05-08 17:21:07 +0530

    added: navbar functionality for mobile screen

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,15 +2,20 @@ import { navItems } from "@/config/navigationItems"
 import useAuthUser from "@/features/user/hooks/useAuthUser"
 import { useState } from "react"
 import { NavLink } from "react-router-dom"
-import { AiOutlineLogout } from "react-icons/ai"
+
+import { FaUserLarge } from "react-icons/fa6"
+import { RxCross2 } from "react-icons/rx"
+import { CgMenuRight } from "react-icons/cg"
 
 const Navbar = () => {
   const { authUser } = useAuthUser()
   const role = authUser?.role || ""
 
+  const [navToggle, setNavToggle] = useState(false)
+
   return (
-    <main className="sticky top-0 z-50 flex h-20 w-full items-center justify-between border bg-card px-5">
-      <h3 className="font-bold text-primary">Tenant flow</h3>
+    <main className="sticky top-0 z-50 flex h-20 w-full items-center justify-between border bg-card">
+      <h3 className="ml-5 font-bold text-primary">Tenant flow</h3>
 
       {/* web nav */}
       <nav className="hidden items-center justify-evenly gap-4 px-5 md:flex">
@@ -33,11 +38,51 @@ const Navbar = () => {
             })}
         </section>
       </nav>
-      <AiOutlineLogout
-        className={`hidden size-8 cursor-pointer rounded-md p-1 hover:bg-danger/80 hover:text-danger-foreground md:block`}
+
+      <nav
+        className="absolute top-20 flex h-[calc(100vh-85px)] w-full flex-col items-center justify-center gap-3 bg-card/95 px-5 md:hidden"
+        style={navToggle ? {} : { display: "none" }}
+      >
+        {navItems
+          ?.filter((item) => item?.rolesAllowed?.includes(role))
+          .map((v, i) => {
+            return (
+              <NavLink
+                to={v.path}
+                className={({ isActive }) =>
+                  `flex h-10 w-full cursor-pointer items-center justify-center gap-4 rounded-md text-sm hover:bg-primary/20 hover:text-primary ${isActive ? "bg-primary/20 text-primary" : ""}`
+                }
+                key={i}
+                onClick={() => setNavToggle(!navToggle)}
+              >
+                <span>{v.icon}</span>
+                <span>{v.label}</span>
+              </NavLink>
+            )
+          })}
+      </nav>
+
+      <FaUserLarge
+        className={`mr-5 hidden size-10 cursor-pointer rounded-full bg-primary p-2.5 text-primary-foreground hover:bg-primary/80 md:block`}
         title="logout"
       />
-      <nav className="flex md:hidden">mobile nav</nav>
+
+      <span
+        className="transition-all ease-in-out active:scale-95 md:hidden"
+        onClick={() => setNavToggle(!navToggle)}
+      >
+        {navToggle ? (
+          <RxCross2
+            className={`mr-5 size-10 cursor-pointer rounded-full bg-primary p-2.5 text-primary-foreground hover:bg-primary/80 md:hidden`}
+            title="logout"
+          />
+        ) : (
+          <CgMenuRight
+            className={`mr-5 size-10 cursor-pointer rounded-full bg-primary p-2.5 text-primary-foreground hover:bg-primary/80 md:hidden`}
+            title="logout"
+          />
+        )}
+      </span>
     </main>
   )
 }
